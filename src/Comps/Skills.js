d1e49d1b5a8e12ad00f429259c9b4f69af0b4225
Jacob Wencloff <jacob.d.wencloff@gmail.com>
2023-03-15 15:12:21 -0700

    Added additional functionality to the skills section, now text corrispoinding to the displayed icon is being displayed

--- src/Comps/Skills.js
+++ src/Comps/Skills.js
@@ -3,16 +3,29 @@ import { useState } from 'react'
 import SkillsCarousel from './SkillsCarousel'
 
 function Skills(props, ref) {
+  const text = {
+    0 : "JavaScript is a scripting or programming language that allows the developer to implement complex features on web pages.",
+    1 : "HTML is the back bone to practically all web based applications giving all supporting languages a canvis to manipulate",
+    2 : "CSS allows the developer to style, format, and animate existing DOM elements",
+    3 : "Python give the developer a fast way to integrate systems effectivly ",
+    4 : "GIT gives its users a form of source controll that allows a user to better manage its current and past working code base's",
+    5 : "AWS IS CRACKED"
+  }
   let [skillsIcons, setSkillsIcon] = useState([])
+  let [displayText, setDisplayText] = useState(text[0])
 
+  const handleSlid = (e) =>{
+    setDisplayText(text[e])
+}
 
   //  console.log(skillsIcons)
   return (
     <div ref={ref} className='skills'>
       <h3>Skills</h3>
       <div id='scroller-window'>
-        <SkillsCarousel />
+        <SkillsCarousel handleSlid={handleSlid} />
       </div>
+      <div id="skillsText">{displayText}</div>
     </div>
   )
 }
--- src/Comps/SkillsCarousel.js
+++ src/Comps/SkillsCarousel.js
@@ -3,12 +3,12 @@ import { Carousel } from 'react-bootstrap/'
 import JSP from "../pictures/JS.png"
 import HTMLP from "../pictures/HTML.png"
 import CSSP from "../pictures/CSS.png"
-import PY3P from "../pictures/AWS.png"
+import PY3P from "../pictures/pythonThumb.png"
 import GITP from "../pictures/GIT.png"
 import AWSP from "../pictures/AWS.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function () {
+export default function (props) {
     const [index, setIndex] = useState(0)
 
     const handleSelect = (selectedIdex, e) => {
@@ -16,26 +16,29 @@ export default function () {
     }
 
 
+
     return (
-        <Carousel onSelect={handleSelect}>
-            <Carousel.Item>
-                <img className='SkillsPic' src={JSP} id="JSP" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className='SkillsPic' src={HTMLP} id="HTMLP" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className='SkillsPic' src={CSSP} id="CSSP" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className='SkillsPic' src={PY3P} id="PY3P" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className='SkillsPic' src={GITP} id="GITP" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className='SkillsPic' src={AWSP} id="AWSP" />
-            </Carousel.Item>
-        </Carousel>
+        <div id="scroller-window">
+            <Carousel onSelect={handleSelect} onSlid={props.handleSlid}>
+                <Carousel.Item>
+                    <img className='SkillsPic' src={JSP} id="JSP" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='SkillsPic' src={HTMLP} id="HTMLP" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='SkillsPic' src={CSSP} id="CSSP" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='SkillsPic' src={PY3P} id="PY3P" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='SkillsPic' src={GITP} id="GITP" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='SkillsPic' src={AWSP} id="AWSP" />
+                </Carousel.Item>
+            </Carousel>
+        </div>
     )
 }
