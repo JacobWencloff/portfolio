fa9ff4d13b7d1cc5097e1e8c8a660c4c32bcea8a
Jacob Wencloff <jacob.d.wencloff@gmail.com>
2023-03-15 14:10:33 -0700

    Removed react Carousel from project, impimented react-bootstraps carousel feature instead due to higher accessibity in the DOM, functionality has been restored to previous capabilites for the time being. Along with this minor css changes have been made including an animation on the home page name

--- src/Comps/Skills.js
+++ src/Comps/Skills.js
@@ -1,54 +1,17 @@
 import React, { forwardRef } from 'react'
 import { useState } from 'react'
-import { Carousel } from 'react-responsive-carousel'
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import JSP from "../pictures/JS.png"
-import HTMLP from "../pictures/HTML.png" 
-import CSSP from "../pictures/CSS.png"
-import PY3P from "../pictures/AWS.png"
-import GITP from "../pictures/GIT.png"
-import AWSP from "../pictures/AWS.png"
+import SkillsCarousel from './SkillsCarousel'
 
 function Skills(props, ref) {
   let [skillsIcons, setSkillsIcon] = useState([])
 
-  let Skills = {
-    'JavaScript': JSP,
-    'HTML': HTMLP,
-    'CSS': CSSP,
-    'Python3': PY3P,
-    'git / SCM': GITP,
-    'AWS Cloud infrastructure': AWSP
-  }
-
-
-
-  skillsIcons = []
-  for (let skill in Skills) {
-    skillsIcons.push(
-      (
-        <div key={skill} className='skill-item'>
-          <img src={Skills[skill]}></img>
-        </div>
-      )
-    )
-  }
 
   //  console.log(skillsIcons)
   return (
     <div ref={ref} className='skills'>
       <h3>Skills</h3>
       <div id='scroller-window'>
-        <Carousel 
-        id='test' 
-        showThumbs={false} 
-        infiniteLoop={true}
-        centerMode={false} 
-        showIndicators={false} 
-        showStatus={false}>
-
-          {skillsIcons}
-        </Carousel>
+        <SkillsCarousel />
       </div>
     </div>
   )
---
+++ src/Comps/SkillsCarousel.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Carousel } from 'react-bootstrap/'
+import JSP from "../pictures/JS.png"
+import HTMLP from "../pictures/HTML.png"
+import CSSP from "../pictures/CSS.png"
+import PY3P from "../pictures/AWS.png"
+import GITP from "../pictures/GIT.png"
+import AWSP from "../pictures/AWS.png"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function () {
+    const [index, setIndex] = useState(0)
+
+    const handleSelect = (selectedIdex, e) => {
+        setIndex(selectedIdex)
+    }
+
+
+    return (
+        <Carousel onSelect={handleSelect}>
+            <Carousel.Item>
+                <img className='SkillsPic' src={JSP} id="JSP" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className='SkillsPic' src={HTMLP} id="HTMLP" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className='SkillsPic' src={CSSP} id="CSSP" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className='SkillsPic' src={PY3P} id="PY3P" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className='SkillsPic' src={GITP} id="GITP" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className='SkillsPic' src={AWSP} id="AWSP" />
+            </Carousel.Item>
+        </Carousel>
+    )
+}
