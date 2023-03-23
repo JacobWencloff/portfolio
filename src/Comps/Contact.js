69e61f7624a012b1ee8be588faf5fefb48c0c8ec
Jacob Wencloff <jacob.d.wencloff@gmail.com>
2023-03-23 14:15:25 -0700

    Fixes

--- src/Comps/Contact.js
+++ src/Comps/Contact.js
@@ -1,40 +1,50 @@
-import React ,{forwardRef} from 'react'
-import  {Form, FloatingLabel, Button }  from 'react-bootstrap'
+import React, { forwardRef, useState } from 'react'
+import { Form, FloatingLabel, Button } from 'react-bootstrap'
 import linkedin from '../pictures/linkedin.png'
 import github from '../pictures/github.png'
 
 function Contact(props, ref) {
+  const [email, setEmail] = useState('')
+
+  function handleSubmit(e){
+    console.log(e)
+  }
+  function handleInputChange(e){
+    console.log(e.target.text)
+  }
+
+
   return (
     <div ref={ref} id='Contact'>
-     <div className='contact'>
-      <h3>Contact Me</h3>
-    <div className='contactForm'>
-        <FloatingLabel
+      <div className='contact'>
+        <h3>Contact Me</h3>
+        <div className='contactForm'>
+          <FloatingLabel
             controlId='floatingEmail'
             label="Email address"
             className='contactForumField'
-            >
-                <Form.Control type='email' placeholder='name@example.com' />
-            </FloatingLabel>
-            <FloatingLabel controlId="floatingFirstName" label='FirstName'>
-                <Form.Control type='Text' placeholder='First Name' />
-            </FloatingLabel>
-            <FloatingLabel controlId='floatingLastName' label='lastName'>
-                <Form.Control type='Text' placeholder='Last Name' />
-            </FloatingLabel>
-            <Button variant='primary' type='submit'>
-              Submit
-            </Button>
-    </div>
-    <div className='contactPics'>
-     <a href='https://www.linkedin.com/in/jacob-wencloff-se/' target='_blank'>
-      <img src={linkedin}></img>
-      </a>
-      <a href='https://github.com/JacobWencloff' target='_blank'>
-        <img src={github}></img>
-      </a>
-    </div>
-    </div>
+          >
+            <Form.Control type='email' placeholder='name@example.com' onChange={handleInputChange}/>
+          </FloatingLabel>
+          <FloatingLabel controlId="floatingFirstName" label='FirstName'>
+            <Form.Control type='Text' placeholder='First Name' />
+          </FloatingLabel>
+          <FloatingLabel controlId='floatingLastName' label='lastName'>
+            <Form.Control type='Text' placeholder='Last Name' />
+          </FloatingLabel>
+          <Button variant='primary' type='submit' onSubmit={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+        <div className='contactPics'>
+          <a href='https://www.linkedin.com/in/jacob-wencloff-se/' target='_blank'>
+            <img src={linkedin}></img>
+          </a>
+          <a href='https://github.com/JacobWencloff' target='_blank'>
+            <img src={github}></img>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Comps/Projects.js
+++ src/Comps/Projects.js
@@ -6,7 +6,7 @@ function Projects(props, ref) {
         <h3 id='project-header'>My Projects</h3>
         <ul id='project-list'>
           <li>
-          <h4>Tasque</h4>
+          <h4>Tasque - <a style={{color: 'black'}} href='https://jacobwencloff.github.io/TasQues/'>click here</a></h4>
           </li>
           <ul>
             <li>A search engine to aggregate and filter results based on user defined constriants.</li>
@@ -14,21 +14,25 @@ function Projects(props, ref) {
             <li>Developed with React leveraging Google search Api.</li>
           </ul>
           <li>
-            <h4>Som-Base</h4>
+            <h4>Som-Base - <a style={{color: 'black'}} href='https://veggie-tales00.github.io/Project3-Client/'>click here</a></h4>
           </li>
           <ul>
             <li>An extensible service providing customer defined food & wine pairings</li>
             <li>Enabling chefs to cultivate a more complete dining experience</li>
             <li>Full stack application built with React, Python, and SQL, hosted on Github Pages and heroku</li>
           </ul>
-          <h4>Algo-Viz</h4>
+          <li>
+          <h4>Algo-Viz - <a style={{color: 'black'}} href='https://jacobwencloff.github.io/Algo-Viz-Client/'>click here</a></h4>
+          </li>
           <ul>
             <li>A visual demonstration of real time data manipluation algorithms</li>
             <li>Provides students a visual representaion of how data is maniplated</li>
             <li>Give's users something great to watch</li>
             <li>Real time graphics developed with Python, OpenGL, and PyImGui</li>
           </ul>
-          <h4>GoatBot.tv SaaS</h4>
+          <li>
+          <h4>GoatBot.tv SaaS - <a style={{color: 'black'}} href='https://gitlab.com/_g0atb0t_' target='_blank'>click here</a> </h4>
+          </li>
           <ul>
             <li>An in-house bot creation studio capable of integrating with multiple platforms</li>
             <li>Allows users to swiftly design and integrate powerful bots with various social platforms</li>
